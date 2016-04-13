1d20d55ebb3cfee9bb5885414356db409554cbf9
federico.manganiello <federico.manganiello@senseisrl.it>
2016-04-13 14:23:21 +0200

    finiti i metodi rest con le query su ehour

---
+++ server/middleware/queryGiorni.js
@@ -0,0 +1,31 @@
+module.exports = function(options) {
+	var mysql = require("mysql");
+	  return function queryGiorni(req, res, next) {
+		// First you need to create a connection to the db
+	  var con = mysql.createConnection({
+	    host: "localhost",
+		user: "federico",
+		database: "ehour"
+	  });
+	
+	
+	  var resList = {results: []};
+	  con.query('select year(ENTRY_DATE) as anno, month(ENTRY_DATE) as mese, round(sum(HOURS)/8,2) as giornateMese from TIMESHEET_ENTRY t join PROJECT_ASSIGNMENT a on t.ASSIGNMENT_ID = a.ASSIGNMENT_ID join PROJECT p on a.PROJECT_ID = p.PROJECT_ID join USERS u on u.USER_ID = a.USER_ID join CUSTOMER c on p.CUSTOMER_ID = c.CUSTOMER_ID group by anno, mese having anno = year(now())', function(err,rows){
+			    if (err) throw err;
+
+			    console.log('queryGiorni performed ...');
+			    console.log(rows);
+			    
+			    var string = JSON.stringify(rows);
+			    console.log('string: ' + string);
+			    resList['results'] = JSON.parse(string);
+			    console.log('resList: ' + JSON.stringify(resList));
+			    res.json(resList);
+			  }
+			);
+	  
+	 
+		  
+	    return res;
+	  }
+};
