c3faaae853c614485999f4dc0d23018e1f4fe6e1
federico.manganiello <federico.manganiello@senseisrl.it>
2016-04-18 12:17:11 +0200

    sistemato lo script di generazione delle tabelle su mongodb

--- server/boot/create-samples-mongodb.js
+++ server/boot/create-samples-mongodb.js
@@ -6,11 +6,18 @@ module.exports = function(app) {
   
   //create all models
   async.parallel({
-	customers: async.apply(createCustomers, function(err, customers) {
-	      if (err) throw err;   	 
-	      console.log('Models created: \n', customers);
-	})
+	customers: async.apply(createCustomers),
+	budgets: async.apply(createBudgets)
+//	customers: async.apply(createCustomers, function(err, customers) {
+//	      if (err) throw err;   	 
+//	      console.log('Customers created: \n', customers);
+//	}),
+//	budgets: async.apply(createBudgets, function(err, budgets) {
+//	      if (err) throw err;   	 
+//	      console.log('Budgets created: \n', budgets);
+//	})
   }, function(err, results) {
+	  console.log('Results: \n', results);
     if (err) {
     	throw err;
     }
@@ -23,7 +30,7 @@ module.exports = function(app) {
   function createCustomers(cb) {
     mongoDs.automigrate('Customer', function(err) {
       if (err) { 
-    	  return cb(err);
+    	  throw err;
       }
       var Customer = app.models.Customer;
       Customer.create([
@@ -36,6 +43,22 @@ module.exports = function(app) {
 	    ], cb);
     });
   }
+  //create budgets
+  function createBudgets(cb) {
+    mongoDs.automigrate('Budget', function(err) {
+      if (err) { 
+    	  return cb(err);
+      }
+      var Budget = app.models.Budget;
+      Budget.create([
+		{id: 1, from: new Date(), to: new Date(), amount: 10000, type: 'Y'},
+		{id: 2, from: new Date(), to: new Date(), amount: 2000, type: 'M'},
+		{id: 3, from: new Date(), to: new Date(), amount: 3000, type: 'M'}
+	    ], cb);
+    });
+  }
+  
+  
   //create projects
   function createProjects(customers, cb) {
     mongoDs.automigrate('Project', function(err) {
