83a0972994b1b376712cd5ca4a1d5f58c6a3308c
federico.manganiello <federico.manganiello@senseisrl.it>
2016-04-28 17:41:22 +0200

    aggiunto metodo remoto per il salvataggio dei budgets

--- common/models/budget.js
+++ common/models/budget.js
@@ -2,10 +2,29 @@ module.exports = function(Budget) {
 //	Budget.validatesInclusionOf('month', {in: ['GEN', 'FEB', 'MAR']});
 //	Budget.validatesInclusionOf('type', {in: ['M', 'Y']});
 //	
-//	Note.greet = function(msg, cb) {
-//	    process.nextTick(function() {
-//	      msg = msg || 'world';
-//	      cb(null, 'Hello ' + msg);
-//	    });
-//	  };
+	
+	Budget.updateAllByProjectId = function(projectId, budgets, cb) {
+		console.log('projectId = ' + projectId);
+		console.log('budgets: ' + JSON.stringify(budgets));
+		
+		if (budgets != null && budgets.length > 0) {
+			console.log('updating budgets...');
+			for (budget in budgets) {
+				console.log(' updating budget: ' + budget);
+				console.log('budget month: ' + budgets[budget].month);
+				Budget.upsert(budgets[budget]);
+			}
+//			Budget.updateAll(budgets);
+		}
+		cb(null, 'Hello ' + projectId);
+	};
+	
+	Budget.remoteMethod(
+		'updateAllByProjectId', 
+		{
+		  accepts:	[{arg: 'projectId', type: 'string'},{arg: 'budgets', type: ['object']}],
+		  returns: {arg: 'greeting', type: 'string'},
+		  http: {path: '/updateAllByProjectId', verb: 'put'}
+		}
+	); 
 };
