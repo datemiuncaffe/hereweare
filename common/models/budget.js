df5a752303ebcc41f0a9513b1393aa29b663fdb6
federico.manganiello <federico.manganiello@senseisrl.it>
2016-04-28 10:00:01 +0200

    aggiunto codice di callback che puo essere utile

--- common/models/budget.js
+++ common/models/budget.js
@@ -1,4 +1,11 @@
 module.exports = function(Budget) {
 //	Budget.validatesInclusionOf('month', {in: ['GEN', 'FEB', 'MAR']});
 //	Budget.validatesInclusionOf('type', {in: ['M', 'Y']});
+//	
+//	Note.greet = function(msg, cb) {
+//	    process.nextTick(function() {
+//	      msg = msg || 'world';
+//	      cb(null, 'Hello ' + msg);
+//	    });
+//	  };
 };
