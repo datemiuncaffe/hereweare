bd3674fbc98bc0a3f355906662e9b3dd9fdee034
federico.manganiello <federico.manganiello@senseisrl.it>
2016-05-06 18:08:00 +0200

    logica per formato data calendario

--- common/models/customer.js
+++ common/models/customer.js
@@ -1,3 +1,8 @@
 module.exports = function(Customer) {
 
+//	Customer.observe('loaded', function(ctx, next) {
+//		console.log('customer name: ', ctx.Customer.name);
+//		next();
+//	});
+	
 };
---
+++ server/boot/hooks.js
@@ -0,0 +1,42 @@
+module.exports = function(app) {
+	// Hooks to /api/customers get method
+	var jp = require('jsonpath');
+	var moment = require('moment');
+	
+	var remotes = app.remotes();
+	remotes.after('Customer.find', function(ctx, next) {
+//		console.log('remotes: ' + JSON.stringify(remotes.toJSON(), null, '\t'));
+//		console.log('ctx result: ', ctx.result);
+		var customers = ctx.result;
+		
+		var res = {
+			customers: ctx.result
+		};		
+		var resFiltered = JSON.parse(JSON.stringify(res));
+		console.log('customer resFiltered: ' + JSON.stringify(res, null, '\t'));
+				
+//		var projects = jp.query(resFiltered, '$.customers[*].projects');
+//		console.log('projects: ' + JSON.stringify(projects, null, '\t'));		
+		
+		jp.apply(resFiltered, '$.customers[*].projects[*].from', function(from) {
+			console.log('from: ' + from + '; typeof: ' + typeof from);
+			if (from != null && from.length > 0) {
+				var formattedfrom = moment(from).format('DD MMM YYYY'); 
+				console.log('formattedfrom: ' + formattedfrom);
+				return formattedfrom;
+			}
+		});
+		jp.apply(resFiltered, '$.customers[*].projects[*].to', function(to) {
+			console.log('to: ' + to + '; typeof: ' + typeof to);
+			if (to != null && to.length > 0) {
+				var formattedto = moment(to).format('DD MMM YYYY'); 
+				console.log('formattedto: ' + formattedto);
+				return formattedto;
+			}
+		});
+		
+		ctx.result = resFiltered.customers;
+
+		next();
+	});
+};
