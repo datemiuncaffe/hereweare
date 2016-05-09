0146feb8bc17fa62aeff9f19180e455ef4a9aa77
federico.manganiello <federico.manganiello@senseisrl.it>
2016-05-09 19:54:41 +0200

    metodo remoto per autocomplete

--- common/models/customer.js
+++ common/models/customer.js
@@ -5,4 +5,29 @@ module.exports = function(Customer) {
 //		next();
 //	});
 	
+	Customer.autocompleteByName = function(name, cb) {
+		console.log('name: ' + name);		
+		if (name != null && name.length > 0) {
+			console.log('autocomplete. name parameter = ' + name);
+			
+			Customer.find({where: {name: {like: '^' + name}}}, function (err, customers) {
+				console.log('err: ' + err);
+				if (err) {
+					console.log('err: ' + err);
+				}
+				console.log('customers: ' + customers);
+				cb(null, {customers: customers});
+			});
+		}		
+	};
+	
+	Customer.remoteMethod(
+		'autocompleteByName', 
+		{
+		  accepts:	[{arg: 'name', type: 'string'}],
+		  returns: {arg: 'res', type: ['object']},
+		  http: {path: '/autocompleteByName', verb: 'get'}
+		}
+	);
+	
 };
