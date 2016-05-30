944b27c7aa31f9bfab618b58271fa774097c837b
federico.manganiello <federico.manganiello@senseisrl.it>
2016-05-30 14:54:05 +0200

    completata procedura di migrazione costi

--- server/middleware/local/ehourmigratecosts.js
+++ server/middleware/local/ehourmigratecosts.js
@@ -7,6 +7,19 @@ module.exports = function(options) {
 		// data sources model
 		var app = req.app;
 		var Project = app.models.Project;
+		var Cost = app.models.Cost;
+		
+		var mongoSequence = require('./../../lib/mongo-sequence');		
+		
+		var con = mysql.createConnection({
+			host : '192.168.88.158',
+			user : 'centos',
+			database : 'ehour'
+		});
+		
+		// sequence
+		var connector = app.dataSources.mongoDs.connector;
+		var db = null;
 
 		var q = async.priorityQueue(migrate, 1);
 		// assign a callback
@@ -27,34 +40,39 @@ module.exports = function(options) {
 			if (err) {
 				console.log('err: ' + err);
 			}
-			console.log('projects: ' + projects);
+			console.log('projects: ' + JSON.stringify(projects));
 			res.json(projects);
 			
-			// process projects
-//			projects.forEach(function(project) {
-//				console.log('project: ' + JSON.stringify(project));
-//				q.push(project, null, function(err) {
-//					console.log('end processing project = ' + project.name);
-//				});
-//			});			
-			
+			connector.connect(function(err, dbase){
+				if (err) {
+					throw err;
+				}
+				db = dbase;
+				
+				// process projects
+				var count = 0;
+				async.whilst(
+				    function () { return count < projects.length; },
+				    function (callback) {
+				    	var project = projects[count];
+				    	console.log('project: ' + JSON.stringify(project));
+						setTimeout(function () {						
+							q.push(project, null, function(err) {
+								console.log('end processing project = ' + project.name);
+							});
+							count++;
+							callback(err, count);
+				        }, 1000);			        
+				    },
+				    function (err, n) {
+				        console.log('end processing projects. number of projects = : ' + n);
+				    }
+				);
+			});			
 		});
-
-		
-//		var mongoSequence = require('./../../lib/mongo-sequence');		
-//		
-		var con = mysql.createConnection({
-			host : '192.168.88.158',
-			user : 'centos',
-			database : 'ehour'
-		});
-//		
-//		// sequence
-//		var connector = app.dataSources.mongoDs.connector;
-//		var db = null;		
 			
 		
-		function migrateCosts(project) {
+		function migrateCosts(project, cb) {
 			console.log('project: ' + project + '; code: ' + project.code);
 			
 			con.query('select year(ENTRY_DATE) as anno, month(ENTRY_DATE) as mese, c.NAME as nomeCliente, p.PROJECT_CODE as codiceProgetto, p.NAME as nomeProgetto, round(sum(HOURS)/8,2) as giornateMese from TIMESHEET_ENTRY t join PROJECT_ASSIGNMENT a on t.ASSIGNMENT_ID = a.ASSIGNMENT_ID join PROJECT p on a.PROJECT_ID = p.PROJECT_ID join CUSTOMER c on p.CUSTOMER_ID = c.CUSTOMER_ID group by anno, mese, c.CUSTOMER_ID, p.PROJECT_ID having codiceProgetto = \'' + project.code + '\' order by anno, mese;', function(err, costs) {
@@ -67,53 +85,47 @@ module.exports = function(options) {
 				    function () { return count < costs.length; },
 				    function (callback) {
 				    	console.log('cost: ' + JSON.stringify(costs[count]));
-				    	count++;
-				        setTimeout(function () {
-				            callback(err, count);
-				        }, 1000);
+				    	var cost = costs[count];
 				    	
-//				    	var projectseq = mongoSequence(db,'projects');
-//						projectseq.getNext(function(err, sequence) {
-//							console.log('projectseq name: ' + projectseq.name + '; no: ' + sequence);
-//							if (err) {
-//								count++;
-//						        setTimeout(function () {
-//						            callback(err, count);
-//						        }, 1000);
-//							} else {
-//								console.log('insert Project with name: ' + projects[count].NAME);
-//								Project.create({
-//									id : sequence,
-//									name : projects[count].NAME,
-//									code : projects[count].PROJECT_CODE,
-//									customerId : customerId
-//								}, function(err, mongoproject) {
-//									if (err) {
-//										throw err;
-//									}
-//									console.log('Project created: \n', JSON.stringify(mongoproject));
-//									count++;
-//							        setTimeout(function () {
-//							            callback(null, count);
-//							        }, 1000);
-//								});
-//							}			
-//						});			    	
-				        
+				    	var costseq = mongoSequence(db,'costs');
+						costseq.getNext(function(err, sequence) {
+							console.log('costseq name: ' + costseq.name + '; no: ' + sequence);
+							if (err) {
+								count++;
+								callback(err, count);
+							} else {
+								console.log('insert Cost. nomeProgetto: ' + cost.nomeProgetto + '; codiceProgetto: ' + cost.codiceProgetto + '; anno: ' + cost.anno + '; mese: ' + cost.mese + '; giornateMese: ' + cost.giornateMese);
+								Cost.create({
+									id : sequence,
+									year : cost.anno,
+									month : cost.mese,
+									days : cost.giornateMese,
+									projectId : project.id
+								}, function(err, mongocost) {
+									if (err) {
+										throw err;
+									}
+									console.log('Cost created: \n', JSON.stringify(mongocost));
+									count++;
+									callback(err, count);
+								});
+							}			
+						});
+				    					        
 				    },
 				    function (err, n) {
 				        console.log('results: ' + n);
+				        cb();
 				    }
 				);
 			});			
 		}
 		
 		function migrate(project, cb) {
-			migrateCosts(project);
-			cb();
+			migrateCosts(project, cb);
 		}
 
 		return res;
 	
-	}  
+	};
 };
