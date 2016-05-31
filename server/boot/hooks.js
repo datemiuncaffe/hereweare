548f12f54430bce603f6fdc84c5bb8155644cca1
federico.manganiello <federico.manganiello@senseisrl.it>
2016-05-31 12:08:08 +0200

    aggiunto filtro sulle date from e to del progetto e dei budgets

--- server/boot/hooks.js
+++ server/boot/hooks.js
@@ -39,4 +39,50 @@ module.exports = function(app) {
 
 		next();
 	});
+	
+	remotes.after('Project.find', function(ctx, next) {
+		var res = {
+			projects: ctx.result
+		};		
+		var resFiltered = JSON.parse(JSON.stringify(res));
+		console.log('project resFiltered: ' + JSON.stringify(res, null, '\t'));
+		
+		jp.apply(resFiltered, '$.projects[*].from', function(from) {
+			console.log('project from: ' + from + '; typeof: ' + typeof from);
+			if (from != null && from.length > 0) {
+				var formattedfrom = moment(from).format('DD MMM YYYY'); 
+				console.log('formattedfrom: ' + formattedfrom);
+				return formattedfrom;
+			}
+		});
+		jp.apply(resFiltered, '$.projects[*].to', function(to) {
+			console.log('project to: ' + to + '; typeof: ' + typeof to);
+			if (to != null && to.length > 0) {
+				var formattedto = moment(to).format('DD MMM YYYY'); 
+				console.log('formattedto: ' + formattedto);
+				return formattedto;
+			}
+		});
+		
+		jp.apply(resFiltered, '$.projects[*].budgets[*].from', function(from) {
+			console.log('budget from: ' + from + '; typeof: ' + typeof from);
+			if (from != null && from.length > 0) {
+				var formattedfrom = moment(from).format('DD MMM YYYY'); 
+				console.log('budget formattedfrom: ' + formattedfrom);
+				return formattedfrom;
+			}
+		});
+		jp.apply(resFiltered, '$.projects[*].budgets[*].to', function(to) {
+			console.log('budget to: ' + to + '; typeof: ' + typeof to);
+			if (to != null && to.length > 0) {
+				var formattedto = moment(to).format('DD MMM YYYY'); 
+				console.log('budget formattedto: ' + formattedto);
+				return formattedto;
+			}
+		});
+		
+		ctx.result = resFiltered.projects;
+
+		next();
+	});
 };
