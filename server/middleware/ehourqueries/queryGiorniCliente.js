e23c61f7c3f1e1d5032918dce5496dde75bcd725
federico.manganiello <federico.manganiello@senseisrl.it>
2016-06-08 15:42:37 +0200

    aggiunti filtri alle chiamate ehour

--- server/middleware/ehourqueries/queryGiorniCliente.js
+++ server/middleware/ehourqueries/queryGiorniCliente.js
@@ -1,31 +1,48 @@
 module.exports = function(options) {
 	var mysql = require("mysql");
-	  return function queryGiorniCliente(req, res, next) {
+	return function queryGiorniCliente(req, res, next) {
 		// First you need to create a connection to the db
-	  var con = mysql.createConnection({
-	    host: "192.168.88.158",
-		user: "centos",
-		database: "ehour"
-	  });
-	
-	
-	  var resList = {results: []};
-	  con.query('select year(ENTRY_DATE) as anno, month(ENTRY_DATE) as mese, c.CUSTOMER_ID as idCliente, c.NAME as nomeCliente, round(sum(HOURS)/8,2) as giornateMese from TIMESHEET_ENTRY t join PROJECT_ASSIGNMENT a on t.ASSIGNMENT_ID = a.ASSIGNMENT_ID join PROJECT p on a.PROJECT_ID = p.PROJECT_ID join USERS u on u.USER_ID = a.USER_ID join CUSTOMER c on p.CUSTOMER_ID = c.CUSTOMER_ID group by anno, mese, c.CUSTOMER_ID having anno = year(now()) and mese = month(now()) - 1', function(err,rows){
-			    if (err) throw err;
+		var con = mysql.createConnection({
+			host : "192.168.88.158",
+			user : "centos",
+			database : "ehour"
+		});
 
-			    console.log('queryGiorniCliente performed ...');
-			    console.log(rows);
-			    
-			    var string = JSON.stringify(rows);
-			    console.log('string: ' + string);
-			    resList['results'] = JSON.parse(string);
-			    console.log('resList: ' + JSON.stringify(resList));
-			    res.json(resList);
-			  }
-			);
-	  
-	 
-		  
-	    return res;
-	  }
+		var resList = {
+			giorni : []
+		};
+		// parse query string parameters
+		var queryparams = req.query;
+		console.log('queryparams: ' + JSON.stringify(queryparams, null, '\t'));
+
+		var query = 'select year(ENTRY_DATE) as anno, month(ENTRY_DATE) as mese, c.CUSTOMER_ID as idCliente, c.NAME as nomeCliente, round(sum(HOURS)/8,2) as giornateMese from TIMESHEET_ENTRY t join PROJECT_ASSIGNMENT a on t.ASSIGNMENT_ID = a.ASSIGNMENT_ID join PROJECT p on a.PROJECT_ID = p.PROJECT_ID join USERS u on u.USER_ID = a.USER_ID join CUSTOMER c on p.CUSTOMER_ID = c.CUSTOMER_ID group by anno, mese, c.CUSTOMER_ID';
+		if (queryparams != null && queryparams.filter != null) {
+			query += ' having ';
+			var keys = Object.keys(queryparams.filter);
+			for (var i = 0; i < keys.length; i++) {
+				query += keys[i] + ' like \'%' + queryparams.filter[keys[i]] + '%\'';
+				if (i !== (keys.length - 1)) {
+					query += ' and ';
+				}
+			}
+		} else {
+			query += ' having anno = year(now()) and mese = month(now()) - 1';
+		}
+		console.log('sql query: ' + JSON.stringify(query, null, '\t'));
+
+		con.query(query, function(err, giorni) {
+			if (err) {
+				throw err;
+			}
+
+			console.log('queryGiorniCliente performed ...');
+			console.log('giorni: ' + JSON.stringify(giorni, null, '\t'));
+
+			resList['giorni'] = giorni;
+			console.log('resList: ' + JSON.stringify(resList));
+			res.json(resList);
+		});
+
+		return res;
+	};
 };
