5c8cfb582a8a7f5d34cc1022f28f14266d2eb7dc
federico.manganiello <federico.manganiello@senseisrl.it>
2016-06-14 15:46:30 +0200

    cambiato il metodo update budgets

--- common/models/budget.js
+++ common/models/budget.js
@@ -1,30 +1,69 @@
 module.exports = function(Budget) {
-//	Budget.validatesInclusionOf('month', {in: ['GEN', 'FEB', 'MAR']});
-//	Budget.validatesInclusionOf('type', {in: ['M', 'Y']});
-//	
 	
-	Budget.updateAllByProjectId = function(projectId, budgets, cb) {
-		console.log('projectId = ' + projectId);
-		console.log('budgets: ' + JSON.stringify(budgets));
+	Budget.updateAllByProjectId = function(project_id, newBudgets, cb) {
+		console.log('new budgets: ' + JSON.stringify(newBudgets, null, '\t') + ' for projectId: ' + project_id);
 		
-		if (budgets != null && budgets.length > 0) {
-			console.log('updating budgets...');
-			for (budget in budgets) {
-				console.log(' updating budget: ' + budget);
-				console.log('budget month: ' + budgets[budget].month);
-				Budget.upsert(budgets[budget]);
-			}
-//			Budget.updateAll(budgets);
+		if (project_id != null && project_id > 0) {
+			Budget.find({where: {projectId: parseInt(project_id)}}, function(err, oldBudgets){
+				if (err) {
+					throw err;
+				}
+				console.log('old budgets: ' + JSON.stringify(oldBudgets, null, '\t'));
+				
+				var newLength = newBudgets.length;
+				var oldLength = oldBudgets.length;
+				if (oldLength > newLength) {
+					var diffLength = oldLength - newLength;
+					newBudgets.forEach(function(currentBudget, i){
+						var oldid = oldBudgets[i].id;
+						oldBudgets[i] = currentBudget;
+						oldBudgets[i].id = oldid;
+					});
+					var oldBudgetsToRemove = oldBudgets.slice(newLength, oldLength);
+					console.log('old budgets to remove: ' + JSON.stringify(oldBudgetsToRemove, null, '\t'));
+					// remove old budgets
+					oldBudgetsToRemove.forEach(function(currentBudget, i){
+						Budget.destroyById(currentBudget.id);		
+					});
+					
+					oldBudgets.splice(newLength, diffLength);
+					console.log('spliced old budgets: ' + JSON.stringify(oldBudgets, null, '\t'));
+					// update old budgets
+					oldBudgets.forEach(function(currentBudget, i){
+						Budget.upsert(currentBudget);						
+					});					
+				} else if (oldLength < newLength) {
+					oldBudgets.forEach(function(currentBudget, i){
+						newBudgets[i].id = currentBudget.id;						
+					});
+					// update new budgets + sequence
+					newBudgets.forEach(function(currentBudget, i){
+						Budget.upsert(currentBudget);						
+					});					
+				} else {
+					newBudgets.forEach(function(currentBudget, i){
+						var oldid = oldBudgets[i].id;
+						oldBudgets[i] = currentBudget;
+						oldBudgets[i].id = oldid;
+					});
+					// update old budgets
+					oldBudgets.forEach(function(currentBudget, i){
+						Budget.upsert(currentBudget);						
+					});
+				}				
+				
+				cb(null, 'projectId ' + project_id);
+				
+			});
 		}
-		cb(null, 'Hello ' + projectId);
 	};
 	
 	Budget.remoteMethod(
 		'updateAllByProjectId', 
 		{
 		  accepts:	[{arg: 'projectId', type: 'string'},{arg: 'budgets', type: ['object']}],
-		  returns: {arg: 'greeting', type: 'string'},
+		  returns: {arg: 'budgets', type: 'string'},
 		  http: {path: '/updateAllByProjectId', verb: 'put'}
 		}
-	); 
+	);
 };
