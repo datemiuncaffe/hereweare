db1c96b28e2d2bcd98c016a3c710327d1fabbf95
federico.manganiello <federico.manganiello@senseisrl.it>
2016-07-29 16:40:16 +0200

    rimosse cartelle di build

--- build/local/server/middleware/log.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(options) {
-  return function log(req, res, next) {
-    var log = {
-    	msg : 'message'
-    };
-    console.log('logging: ' + log);
-    return res.json(log);
-  }
-};
--- build/local/shipitfile.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./config/shipit').init;
