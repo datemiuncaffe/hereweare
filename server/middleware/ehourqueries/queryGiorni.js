8245aa86e7611a91db1aff16231b123b7220fea4
federico.manganiello <federico.manganiello@senseisrl.it>
2016-09-20 17:04:37 +0200

    tolte le assenze dalla query giornate erogate

--- server/middleware/ehourqueries/queryGiorni.js
+++ server/middleware/ehourqueries/queryGiorni.js
@@ -11,7 +11,7 @@ module.exports = function(options) {
 		var queryparams = req.query;
 		logger.info('queryparams: ' + JSON.stringify(queryparams, null, '\t'));
 
-		var query = 'select year(ENTRY_DATE) as anno, month(ENTRY_DATE) as mese, round(sum(HOURS)/8,2) as giornateMese from TIMESHEET_ENTRY t join PROJECT_ASSIGNMENT a on t.ASSIGNMENT_ID = a.ASSIGNMENT_ID join PROJECT p on a.PROJECT_ID = p.PROJECT_ID join USERS u on u.USER_ID = a.USER_ID join CUSTOMER c on p.CUSTOMER_ID = c.CUSTOMER_ID group by anno, mese';
+		var query = 'select year(ENTRY_DATE) as anno, month(ENTRY_DATE) as mese, round(sum(HOURS)/8,2) as giornateMese from TIMESHEET_ENTRY t join PROJECT_ASSIGNMENT a on t.ASSIGNMENT_ID = a.ASSIGNMENT_ID join PROJECT p on a.PROJECT_ID = p.PROJECT_ID join USERS u on u.USER_ID = a.USER_ID join CUSTOMER c on p.CUSTOMER_ID = c.CUSTOMER_ID where c.CODE != \'ASS\' group by anno, mese';
 		if (queryparams != null && queryparams.filter != null) {
 			query += ' having ';
 			var keys = Object.keys(queryparams.filter);
