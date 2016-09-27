4035cbcbb6a9a4498d43129352142131eeab8f8a
federico.manganiello <federico.manganiello@senseisrl.it>
2016-09-27 11:01:32 +0200

    servizio report dipendenti

---
+++ server/middleware/ehourqueries/queryReportsByUserIdAndDateInterval.js
@@ -0,0 +1,58 @@
+module.exports = function(options) {
+	var mysql = require("mysql");
+	var MysqlPool = require('./../../lib/mysql-pool').pool();
+	var logger = require('./../../lib/logger');
+
+	return function queryReportsByUserIdAndDateInterval(req, res, next) {
+		var query = 'select t.ENTRY_DATE as entrydate, ' +
+				'c.NAME as nomeCliente, ' +
+				'p.NAME as nomeProgetto, p.PROJECT_CODE as codiceProgetto, ' +
+				'concat(u.LAST_NAME, \' - \', u.FIRST_NAME) as dipendente, ' +
+				's.ROLE as role, t.COMMENT as Commento, t.HOURS as ore ' +
+				'from TIMESHEET_ENTRY t ' +
+				'join PROJECT_ASSIGNMENT a on t.ASSIGNMENT_ID = a.ASSIGNMENT_ID ' +
+				'join PROJECT p on a.PROJECT_ID = p.PROJECT_ID ' +
+				'join USERS u on u.USER_ID = a.USER_ID ' +
+				'join USER_TO_USERROLE s on s.USER_ID = u.USER_ID ' +
+				'join CUSTOMER c on p.CUSTOMER_ID = c.CUSTOMER_ID ';
+
+		// parse query string parameters
+		var queryparams = req.query;
+		logger.info('queryparams: ' + JSON.stringify(queryparams, null, '\t'));
+
+		if (queryparams.lastName == null ) {
+			res.json([]);
+		} else {
+			query += 'where u.LAST_NAME = \'' + queryparams.lastName + '\'';
+			if (queryparams.startDate != null ) {
+				query += ' and t.ENTRY_DATE > \'' + queryparams.startDate + '\'';
+			}
+			if (queryparams.endDate != null ) {
+				query += ' and t.ENTRY_DATE < \'' + queryparams.endDate + '\'';
+			}
+			query += ' order by t.ENTRY_DATE;';
+			logger.info('sql query: ' + JSON.stringify(query, null, '\t'));
+			MysqlPool.getConnection(getData, query);
+		}
+
+		function getData(err, connection, query) {
+			connection.query(query, function(err, reports) {
+				if (err) {
+					logger.info('err: ' + JSON.stringify(err));
+					MysqlPool.releaseConnection(connection);
+					throw err;
+				}
+
+				MysqlPool.releaseConnection(connection);
+				logger.info('queryReportsByUserIdAndDateInterval performed ...');
+				logger.info('reports: ' +
+					JSON.stringify(reports, null, '\t'));
+
+				res.json(reports);
+			});
+		};
+
+		return res;
+
+	};
+};
