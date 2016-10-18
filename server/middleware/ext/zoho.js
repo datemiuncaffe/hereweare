d48e58cbac10e1af0a1b9dc505d0b59aa2bb2afe
federico.manganiello <federico.manganiello@senseisrl.it>
2016-10-18 12:34:32 +0200

    chiamata autenticata zoho

--- server/middleware/ext/zoho.js
+++ server/middleware/ext/zoho.js
@@ -3,33 +3,30 @@ module.exports = function(options) {
 	var request = require('request');
 
 	return function zoho(req, res, next) {
-		var params = "SCOPE=ZohoProjects/projectsapi";
-		//--post-data="lid=federico.manganiello@senseisrl.it&pwd=10pietro"
-		var url = "https://accounts.zoho.com/apiauthtoken/create?";
-		url += params;
-
-		var formData = {
-			lid: "federico.manganiello@senseisrl.it",
-			pwd: "10pietro"
+		var url = "https://accounts.zoho.com/apiauthtoken/nb/create?";
+		var params = {
+			SCOPE: "ZohoProjects/projectsapi",
+			EMAIL_ID: "federico.manganiello@senseisrl.it",
+			PASSWORD: "10pietro"
 		};
 
-		request.post({
+		request.get({
 				url: url,
-				formData: formData
+				qs: params
 			}, function (err, resp, body) {
-			//logger.info('response: ' + resp);
-			logger.info('error: ' + err);
-			var result = {
-				response: resp,
-				error: err
-			};
+				//logger.info('response: ' + resp);
+				logger.info('error: ' + err);
+				var result = {
+					response: resp,
+					error: err
+				};
 
-			//logger.info(resp);
-			if (!err && resp.statusCode == 200) {
-		    logger.info(body); // Show the HTML for the Google homepage.
-		  }
-			res.json(result);
-			//res.json({msg : 'ok'});
+				//logger.info(resp);
+				if (!err && resp.statusCode == 200) {
+			    logger.info(body); // Show the HTML for the Google homepage.
+			  }
+				res.json(result);
+				//res.json({msg : 'ok'});
 		});
 
 		return res;
