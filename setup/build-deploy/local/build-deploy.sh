b00ab369d747ca5d510dc8af8fe7b6414000c945
federico.manganiello <federico.manganiello@senseisrl.it>
2017-01-23 15:46:05 +0100

    script di shell build-deploy

---
+++ setup/build-deploy/local/build-deploy.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# hereweare - build-deploy script for hereweare
+# run as:
+# bash -x build-deploy.sh buildcomplete
+#
+
+node=${NODE-/usr/local/bin/node}
+
+build()
+{
+  echo -n $"building hereweare ..."
+  gulp --cwd ./../../../ build
+  RETVAL=$?
+  echo [ $RETVAL -eq 0 ]
+}
+
+buildcomplete()
+{
+  echo -n $"full building hereweare ..."
+  gulp --cwd ./../../../ buildcomplete
+  RETVAL=$?
+  echo [ $RETVAL -eq 0 ]
+}
+
+clean()
+{
+  echo -n $"cleaning hereweare build folder ..."
+  gulp --cwd ./../../../ clean
+  RETVAL=$?
+  echo [ $RETVAL -eq 0 ]
+}
+
+deploy()
+{
+  echo -n $"deploying hereweare to ehour machine ..."
+  RETVAL=$?
+  echo [ $RETVAL -eq 0 ]
+}
+
+RETVAL=0
+case "$1" in
+  build)
+    build
+    ;;
+  buildcomplete)
+    buildcomplete
+    ;;
+  clean)
+    clean
+    ;;
+  deploy)
+    deploy
+    ;;
+  status)
+    status $node
+    RETVAL=$?
+    ;;
+  *)
+    echo "Usage: $0 {build|buildcomplete|clean|deploy|status}"
+    RETVAL=1
+esac
+exit $RETVAL
