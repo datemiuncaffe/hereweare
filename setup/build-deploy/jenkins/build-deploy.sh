fc1b35b93c5d46065df7df329cce8529890f4199
federico.manganiello <federico.manganiello@senseisrl.it>
2017-01-24 15:29:29 +0100

    spript jenkins: check gulp

--- setup/build-deploy/jenkins/build-deploy.sh
+++ setup/build-deploy/jenkins/build-deploy.sh
@@ -11,7 +11,7 @@ build()
   echo -n $"building hereweare ..."
   npm list gulp
   GULP_CHECK=$?
-  if [ ! GULP_CHECK -eq 0 ]; then
+  if [ ! (GULP_CHECK -eq 0) ]; then
     npm install --save gulp
   fi
   gulp --cwd ./../../../ build
