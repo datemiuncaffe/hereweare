83174e8f95c53603b9056c0a5ee8108c4ecc3ad8
federico manganiello <federico.manganiello@senseisrl.it>
2017-03-08 16:34:43 +0100

    aggiunto task analisi sonar

--- setup/build-deploy/jenkins/build-deploy.sh
+++ setup/build-deploy/jenkins/build-deploy.sh
@@ -10,82 +10,94 @@ PACKAGE_NAME="hereweare-frontend"
 
 installDeps()
 {
-  echo -n $"installing $PACKAGE_NAME dependencies via npm ..."
-  npm install
+   echo -n $"installing $PACKAGE_NAME dependencies via npm ..."
+   npm install
 }
 
 build()
 {
-  echo -n $"building $PACKAGE_NAME ..."
-  #npm list gulp
-  #GULP_CHECK=$?
-  #if [ ! $GULP_CHECK -eq 0 ]; then
-  #  npm install --save gulp
-  #fi
-  #gulp --cwd ./../../../ build
-  installDeps
-  gulp build
-  RETVAL=$?
-  echo [ $RETVAL -eq 0 ]
+   echo -n $"building $PACKAGE_NAME ..."
+   #npm list gulp
+   #GULP_CHECK=$?
+   #if [ ! $GULP_CHECK -eq 0 ]; then
+   #  npm install --save gulp
+   #fi
+   #gulp --cwd ./../../../ build
+   installDeps
+   gulp build
+   RETVAL=$?
+   echo [ $RETVAL -eq 0 ]
 }
 
 buildcomplete()
 {
-  echo -n $"full building $PACKAGE_NAME ..."
-  installDeps
-  gulp buildcomplete
-  RETVAL=$?
-  echo [ $RETVAL -eq 0 ]
+   echo -n $"full building $PACKAGE_NAME ..."
+   installDeps
+   gulp buildcomplete
+   RETVAL=$?
+   echo [ $RETVAL -eq 0 ]
 }
 
 clean()
 {
-  echo -n $"cleaning $PACKAGE_NAME build folder ..."
-  gulp clean
-  RETVAL=$?
-  echo [ $RETVAL -eq 0 ]
+   echo -n $"cleaning $PACKAGE_NAME build folder ..."
+   gulp clean
+   RETVAL=$?
+   echo [ $RETVAL -eq 0 ]
 }
 
 deploy()
 {
-  echo -n $"deploying $PACKAGE_NAME to ehour machine ..."
-  gulp deploy
-  RETVAL=$?
-  echo [ $RETVAL -eq 0 ]
+   echo -n $"deploying $PACKAGE_NAME to ehour machine ..."
+   gulp deploy
+   RETVAL=$?
+   echo [ $RETVAL -eq 0 ]
 }
 
 delivery-pipeline()
 {
-  echo -n $"$PACKAGE_NAME delivery pipeline ..."
-  installDeps
-  gulp delivery-pipeline
-  RETVAL=$?
-  echo [ $RETVAL -eq 0 ]
+   echo -n $"$PACKAGE_NAME delivery pipeline ..."
+   installDeps
+   gulp delivery-pipeline
+   RETVAL=$?
+   echo [ $RETVAL -eq 0 ]
+}
+
+sonar-pipeline()
+{
+   echo -n $"$PACKAGE_NAME sonar pipeline ..."
+   installDeps
+   gulp sonar-pipeline:test
+   RETVAL=$?
+   echo [ $RETVAL -eq 0 ]
 }
 
 RETVAL=0
 case "$1" in
-  build)
-    build
-    ;;
-  buildcomplete)
-    buildcomplete
-    ;;
-  clean)
-    clean
-    ;;
-  deploy)
-    deploy
-    ;;
-  delivery-pipeline)
-    delivery-pipeline
-    ;;
-  status)
-    status $node
-    RETVAL=$?
-    ;;
-  *)
-    echo "Usage: $0 {build|buildcomplete|clean|deploy|delivery-pipeline|status}"
-    RETVAL=1
+   build)
+      build
+      ;;
+   buildcomplete)
+      buildcomplete
+      ;;
+   clean)
+      clean
+      ;;
+   deploy)
+      deploy
+      ;;
+   delivery-pipeline)
+      delivery-pipeline
+      ;;
+   sonar-pipeline)
+      sonar-pipeline:test
+      ;;
+   status)
+      status $node
+      RETVAL=$?
+      ;;
+   *)
+      echo "Usage: $0 {build|buildcomplete|clean|deploy|delivery-pipeline|sonar-pipeline|status}"
+      RETVAL=1
 esac
 exit $RETVAL
