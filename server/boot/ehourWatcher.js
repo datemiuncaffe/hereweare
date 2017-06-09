976329c25bea84b13cdd51c263d77467d94ce86a
federico manganiello <federico.manganiello@senseisrl.it>
2017-06-09 10:43:25 +0200

    disabilitato ehourWatcher

--- server/boot/ehourWatcher.js
+++ server/boot/ehourWatcher.js
@@ -9,30 +9,30 @@ var conProps = {
 
 module.exports = function(app) {
   logger.info("event");
-  var eventWatcher = MySQLEvents(conProps);
+  // var eventWatcher = MySQLEvents(conProps);
+  //
+  // var watcher = eventWatcher.add(
+  //   //'ehour.tehourWatcherable.field.value',
+  //   'ehour',
+  //   function (oldRow, newRow) {
+  //     logger.info("ehour event ...");
+  //      //row inserted
+  //     if (oldRow === null) {
+  //       //insert code goes here
+  //     }
+  //
+  //      //row deleted
+  //     if (newRow === null) {
+  //       //delete code goes here
+  //     }
+  //
+  //      //row updated
+  //     if (oldRow !== null && newRow !== null) {
+  //       //update code goes here
+  //     }
+  //   }
+  //   //'match this string or regex'
+  // );
 
-  var watcher = eventWatcher.add(
-    //'ehour.tehourWatcherable.field.value',
-    'ehour',
-    function (oldRow, newRow) {
-      logger.info("ehour event ...");
-       //row inserted
-      if (oldRow === null) {
-        //insert code goes here
-      }
-
-       //row deleted
-      if (newRow === null) {
-        //delete code goes here
-      }
-
-       //row updated
-      if (oldRow !== null && newRow !== null) {
-        //update code goes here
-      }
-    }
-    //'match this string or regex'
-  );
-
-  logger.info("event: ");
+  //logger.info("event: ");
 };
