acda7a0e2a7be20ae93c876a83d0212d9f11db19
federico manganiello <federico.manganiello@senseisrl.it>
2017-09-04 19:14:33 +0200

    aggiunto shipit task clientInstall

--- config/shipit-delivery.js
+++ config/shipit-delivery.js
@@ -3,7 +3,7 @@ var config = {
 	  	workspace: '.',
 	  	dirToCopy: 'build/test',
 	  	deployTo: '/opt/hereweare-frontend',
-		ignores: ['.git', 'client/vendor', 'node_modules'],
+		ignores: ['.git', 'client/vendor', 'client/node_modules', 'node_modules'],
 		keepReleases: 5,
 	  	key: '/home/tomcat7/.ssh/id_rsa_sensei',
 		branch: 'remotes/origin/nuovolayout'
@@ -20,9 +20,35 @@ module.exports.init = function(shipit) {
 
 	shipit.initConfig(config);
 
-	shipit.task('pwd', function () {
-		return shipit.remote('pwd');
+	shipit.task('clientInstall', function () {
+		shipit.log('dir: ' + shipit.releasePath);
+		shipit.log('dir: ' + shipit.currentPath);
+
+		var npm_install_dir = shipit.config.deployTo + '/current/client';
+		if (shipit.releasePath) {
+			npm_install_dir = shipit.releasePath + '/client';
+		}
+		if (shipit.currentPath) {
+			npm_install_dir = shipit.currentPath + '/client';
+		}
+
+		var cmd = 'cd ' + npm_install_dir + ' && npm install';
+
+		return shipit.remote(cmd)
+				.then(function (res) {
+        			shipit.emit('clientInstall');
+					shipit.log('finished to install client dependencies');
+      		});
+
 	});
+
+	shipit.task('pwd', function () {
+		return shipit.remote('pwd')
+				.then(function () {
+					shipit.emit('pwd');
+				});
+	});
+	
 	shipit.task('list', function () {
 		return shipit.remote('ls -la');
 	});
